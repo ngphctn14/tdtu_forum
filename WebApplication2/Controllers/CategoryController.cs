7924024ccaf0dd14b3fa1a29921b9c5c37203b38
Phuc Tan Ngo <ngophuctan1407@gmail.com>
2024-10-08 14:54:52 +0700

    update

---
+++ WebApplication2/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        ForumnManagerEntities _db = new ForumnManagerEntities();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getDetails()
+        {
+            var v = from t in _db.categories
+                    select t;
+            return PartialView(v.ToList());
+        }
+
+        public ActionResult getDetailsSelect()
+        {
+            var v = from t in _db.categories
+                    select t;
+            return PartialView(v.ToList());
+        }
+    }
+}
---
+++ WebApplication2/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PostController : Controller
+    {
+        ForumnManagerEntities _db = new ForumnManagerEntities();
+        // GET: Post
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
