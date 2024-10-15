4dc2bd3a40fc3a8a291a8c87b8a0370644ab18cb
LMHoang-3003 <vatcvietmy123456@gmail.com>
2024-10-15 16:50:29 +0700

    update

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication2.Controllers;
+using WebApplication2.Models;
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        ForumnManagerEntities _db = new ForumnManagerEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,14 @@ namespace WebApplication2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        //Co gi vo sql them bang welcome_home 
+        public ActionResult getHomeWelcome()
+        {
+            var v = from t in _db.welcome_home
+                    select t;
+            return PartialView(v.ToList());
         }
     }
 }
