c2c907f63bfa5c16637cf651c432a5f155ce5e93
LMHoang-3003 <vatcvietmy123456@gmail.com>
2024-11-26 13:15:55 +0700

    update notification user category

--- WebApplication2/Areas/admin/Controllers/categoriesController.cs
+++ WebApplication2/Areas/admin/Controllers/categoriesController.cs
@@ -10,107 +10,107 @@ using WebApplication2.Models;
 
 namespace WebApplication2.Areas.admin.Controllers
 {
-    public class welcome_homeController : Controller
+    public class categoriesController : Controller
     {
         private ForumnManagerEntities6 db = new ForumnManagerEntities6();
 
-        // GET: admin/welcome_home
+        // GET: admin/categories
         public ActionResult Index()
         {
-            return View(db.welcome_home.ToList());
+            return View(db.categories.ToList());
         }
 
-        // GET: admin/welcome_home/Details/5
+        // GET: admin/categories/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            welcome_home welcome_home = db.welcome_home.Find(id);
-            if (welcome_home == null)
+            category category = db.categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            return View(welcome_home);
+            return View(category);
         }
 
-        // GET: admin/welcome_home/Create
+        // GET: admin/categories/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: admin/welcome_home/Create
+        // POST: admin/categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "welcome_id,welcome_title,link, hide")] welcome_home welcome_home)
+        public ActionResult Create([Bind(Include = "category_id,name")] category category)
         {
             if (ModelState.IsValid)
             {
-                db.welcome_home.Add(welcome_home);
+                db.categories.Add(category);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(welcome_home);
+            return View(category);
         }
 
-        // GET: admin/welcome_home/Edit/5
+        // GET: admin/categories/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            welcome_home welcome_home = db.welcome_home.Find(id);
-            if (welcome_home == null)
+            category category = db.categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            return View(welcome_home);
+            return View(category);
         }
 
-        // POST: admin/welcome_home/Edit/5
+        // POST: admin/categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "welcome_id,welcome_title,link, hide")] welcome_home welcome_home)
+        public ActionResult Edit([Bind(Include = "category_id,name")] category category)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(welcome_home).State = System.Data.Entity.EntityState.Modified;
+                db.Entry(category).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(welcome_home);
+            return View(category);
         }
 
-        // GET: admin/welcome_home/Delete/5
+        // GET: admin/categories/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            welcome_home welcome_home = db.welcome_home.Find(id);
-            if (welcome_home == null)
+            category category = db.categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            return View(welcome_home);
+            return View(category);
         }
 
-        // POST: admin/welcome_home/Delete/5
+        // POST: admin/categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            welcome_home welcome_home = db.welcome_home.Find(id);
-            db.welcome_home.Remove(welcome_home);
+            category category = db.categories.Find(id);
+            db.categories.Remove(category);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
